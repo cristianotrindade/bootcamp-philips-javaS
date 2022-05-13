105ae18dd4f05d5fc28266f01c973ca87f8e02be
cristianotrindade <cristiano.trinda@gmail.com>
2022-05-13 19:33:33 -0300

    Bootcamp Philips - aula de javaScript

---
+++ javaModulo/funcoes.mjs
@@ -0,0 +1,20 @@
+function mostraIdade(nome, idade){
+    return `A idade de ${nome} é ${idade}`;
+}
+
+function mostraCidade(nome, cidade) {
+    return ` A cidade de ${nome} é ${cidade}`;
+
+}
+
+function mostraHobby( nome, hobby){
+    return ` O hobby de ${nome} é ${hobby}`;
+}
+
+function meuCarro(nome, ano){
+    return ` Meu carro é um ${nome} do ano ${ano}`;
+
+}
+
+
+export {mostraIdade, mostraCidade, mostraHobby, meuCarro};
---
+++ javaModulo/main.mjs
@@ -0,0 +1,6 @@
+import { mostraCidade, mostraIdade, mostraHobby, meuCarro } from "./funcoes.mjs";
+
+console.log(mostraIdade("Cristiano", 42));
+console.log(mostraCidade("Cristiano", "SJCampos"));
+console.log(mostraHobby("Cristiano", "musculação"));
+console.log(meuCarro("Polo", 2002));
